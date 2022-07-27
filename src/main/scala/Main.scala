ec97b3a1de0aff7f85f375ebc79d132c6b0e2924
mm1995tk <jimi.woodstock1995@gmail.com>
2022-07-27 21:01:47 +0900

    メインの追加

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
 @main def repl: Unit =
-  val lexer = Lexer.ap("app")
-  println(lexer.get_cur)
-  println(lexer.read_char.read_char.get_cur)
+  val lexer = Lexer.from("app")
+  // println(lexer.get_cur_ch)
+  // println(lexer.read_char.read_char.get_cur_ch)
