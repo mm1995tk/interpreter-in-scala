bc4b2b49f53a4bcc9e0092a6f3990922cac5e08d
mm1995tk <jimi.woodstock1995@gmail.com>
2022-07-28 20:06:24 +0900

    2文字以上から成るトークンに対応

--- src/main/scala/Lexer.scala
+++ src/main/scala/Lexer.scala
@@ -1,52 +1,55 @@
 case class Lexer(input: String, cursor: Int = 0):
-  def next(i: Int = 0) = advanceCursorRecursive(i, this.advanceCursor)
+  def getToken =
+    val next = this.next
+    this.getChar match
+      case '=' =>
+        if next.getChar == '=' then (next.next, Token.EQ)
+        else (next, Token.ASSIGN)
+      case '!' =>
+        if next.getChar == '=' then (next.next, Token.NotEQ)
+        else (next, Token.BANG)
+      case '+'                  => (next, Token.PLUS)
+      case '('                  => (next, Token.LPAREN)
+      case ')'                  => (next, Token.RPAREN)
+      case '{'                  => (next, Token.LBRACE)
+      case '}'                  => (next, Token.RBRACE)
+      case ','                  => (next, Token.COMMA)
+      case ';'                  => (next, Token.SEMICOLON)
+      case ch if ch == 0.toChar => (next, Token.EOF)
+      case _                    => (next, Token.ILLEGAL)
 
-  def getNextToken = this.getChar map {
-    case '=' => Token.ASSIGN
-    case '+' => Token.PLUS
-    case '(' => Token.LPAREN
-    case ')' => Token.RPAREN
-    case '{' => Token.LBRACE
-    case '}' => Token.RBRACE
-    case ',' => Token.COMMA
-    case ';' => Token.SEMICOLON
-    case _   => Token.ILLEGAL
-  }
+  private def next = this.advanceCursor.skipWhitespace
+  // def next(i: Int = 0) = advanceCursorRecursive(i, this.advanceCursor)
 
-  private def skipWhitespace: Option[Lexer] =
-    this.getChar match {
-      case Some(ch) if ch.isAsciiWhitespace =>
-        this.advanceCursor.flatMap(_.skipWhitespace)
-      case _ => Some(this)
-    }
+  private def skipWhitespace: Lexer =
+    if this.getChar.isAsciiWhitespace then this.advanceCursor.skipWhitespace
+    else this
 
   private def getChar =
-    if this.cursor == 0 then None
-    else Some(this.input.charAt(this.cursor - 1))
+    if this.cursor > this.input.length - 1 then 0.toChar
+    else this.input.charAt(this.cursor)
 
-  private def advanceCursor =
-    if this.hasNext then Some(Lexer(this.input, this.cursor + 1))
-    else None
+  private def advanceCursor = Lexer(this.input, this.cursor + 1)
 
-  private def len = this.input.length()
-  private def hasNext = this.cursor < this.len
+  // private def advanceCursorRecursive(
+  //     i: Int,
+  //     item: Lexer
+  // ): Lexer =
+  //   if i == 0 then item.skipWhitespace
+  //   else
+  //     advanceCursorRecursive(
+  //       i - 1,
+  //       item.skipWhitespace.advanceCursor
+  //     )
 
-  private def advanceCursorRecursive(
-      i: Int,
-      item: Option[Lexer]
-  ): Option[Lexer] =
-    if i == 0 then item.flatMap(_.skipWhitespace)
-    else
-      advanceCursorRecursive(
-        i - 1,
-        item.flatMap(_.skipWhitespace.flatMap(_.advanceCursor))
-      )
-
-object Lexer {
-  def from(input: String): Lexer = Lexer(input)
-}
+object Lexer:
+  def from(input: String): Lexer = Lexer(input).skipWhitespace
 
 extension (item: Char)
   def isAsciiWhitespace: Boolean = item match
     case ' ' | '\n' | '\t' | '\r' => true
     case _                        => false
+
+  def isMaybeCharNeedToSkip = item match
+    case '=' | '!' => true
+    case _         => false
--- src/test/scala/LexerTest.scala
+++ src/test/scala/LexerTest.scala
@@ -1,10 +1,40 @@
 class LexerTest extends munit.FunSuite {
-  test("next token") {
-    val input = " = +(  ){ },;p"
-    val lexer = Lexer.from(input)
+  test("単一文字で形成されるトークン") {
+    val input = " = +(  ){ },=;p"
+    var lexer = Lexer.from(input)
 
     for (
       (token, index) <- Seq(
+        Token.ASSIGN,
+        Token.PLUS,
+        Token.LPAREN,
+        Token.RPAREN,
+        Token.LBRACE,
+        Token.RBRACE,
+        Token.COMMA,
+        Token.ASSIGN,
+        Token.SEMICOLON,
+        Token.ILLEGAL
+      ).zipWithIndex
+    ) {
+      val (nextLexer, curToken) = lexer.getToken
+
+      assertEquals(curToken, token)
+      lexer = nextLexer
+    }
+    val (nextLexer, curToken) = lexer.getToken
+
+    assertEquals(curToken, Token.EOF)
+    lexer = nextLexer
+
+  }
+  test("==を含む") {
+    val input = " === +(  ){ },;p"
+    var lexer = Lexer.from(input)
+
+    for (
+      (token, index) <- Seq(
+        Token.EQ,
         Token.ASSIGN,
         Token.PLUS,
         Token.LPAREN,
@@ -16,8 +46,46 @@ class LexerTest extends munit.FunSuite {
         Token.ILLEGAL
       ).zipWithIndex
     ) {
+      val (nextLexer, curToken) = lexer.getToken
 
-      assertEquals(lexer.next(index).get.getNextToken.get, token)
+      assertEquals(curToken, token)
+      lexer = nextLexer
     }
+    val (nextLexer, curToken) = lexer.getToken
+
+    assertEquals(curToken, Token.EOF)
+    lexer = nextLexer
+
+  }
+
+  test("！=を含む") {
+    val input = " != +( ==0 ){ },;p"
+    var lexer = Lexer.from(input)
+
+    for (
+      (token, index) <- Seq(
+        Token.NotEQ,
+        Token.PLUS,
+        Token.LPAREN,
+        Token.EQ,
+        Token.ILLEGAL,
+        Token.RPAREN,
+        Token.LBRACE,
+        Token.RBRACE,
+        Token.COMMA,
+        Token.SEMICOLON,
+        Token.ILLEGAL
+      ).zipWithIndex
+    ) {
+      val (nextLexer, curToken) = lexer.getToken
+
+      assertEquals(curToken, token)
+      lexer = nextLexer
+    }
+    val (nextLexer, curToken) = lexer.getToken
+
+    assertEquals(curToken, Token.EOF)
+    lexer = nextLexer
+
   }
 }
