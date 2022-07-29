2c877a7af4a7616890db059c02b6eac429bbece4
mm1995tk <jimi.woodstock1995@gmail.com>
2022-07-30 04:06:30 +0900

    テスト用ファイルの字句解析ができるように修正

--- src/main/scala/Lexer.scala
+++ src/main/scala/Lexer.scala
@@ -1,25 +1,40 @@
 case class Lexer(input: String, cursor: Int = 0):
-  def getToken =
+  def getToken: (Lexer, Token) =
     val next = this.next
     this.getChar match
       case '=' =>
         if next.getChar == '=' then (next.next, Token.EQ)
-        else (next, Token.ASSIGN)
+        else next -> Token.ASSIGN
+
       case '!' =>
         if next.getChar == '=' then (next.next, Token.NotEQ)
-        else (next, Token.BANG)
-      case '+' => (next, Token.PLUS)
-      case '(' => (next, Token.LPAREN)
-      case ')' => (next, Token.RPAREN)
-      case '{' => (next, Token.LBRACE)
-      case '}' => (next, Token.RBRACE)
-      case ',' => (next, Token.COMMA)
-      case ';' => (next, Token.SEMICOLON)
-      case 0   => (next, Token.EOF)
-      case _   => (next, Token.ILLEGAL)
+        else next -> Token.BANG
+
+      case ch: CodeLiteral => next -> ch.convertCharOfCodeToToken
+
+      case ch if ch.isDigit =>
+        val (lexer, token) = this.readNumber()
+        lexer.skipWhitespace -> token
+
+      case ch if ch.isLetter => this.getKeyWordToken
+      case 0                 => next -> Token.EOF
+      case _                 => next -> Token.ILLEGAL
+
+  private def getKeyWordToken =
+    val (lexer, ident) = this.readIdentifier()
+    lexer.skipWhitespace -> {
+      ident match
+        case "let"    => Token.LET
+        case "return" => Token.RETURN
+        case "if"     => Token.IF
+        case "else"   => Token.ELSE
+        case "true"   => Token.TRUE
+        case "false"  => Token.FALSE
+        case "fn"     => Token.FUNCTION
+        case ch       => Token.IDENT(ch)
+    }
 
   private def next = this.advanceCursor.skipWhitespace
-  // def next(i: Int = 0) = advanceCursorRecursive(i, this.advanceCursor)
 
   private def skipWhitespace: Lexer =
     if this.getChar.isAsciiWhitespace then this.advanceCursor.skipWhitespace
@@ -31,25 +46,37 @@ case class Lexer(input: String, cursor: Int = 0):
 
   private def advanceCursor = Lexer(this.input, this.cursor + 1)
 
-  // private def advanceCursorRecursive(
-  //     i: Int,
-  //     item: Lexer
-  // ): Lexer =
-  //   if i == 0 then item.skipWhitespace
-  //   else
-  //     advanceCursorRecursive(
-  //       i - 1,
-  //       item.skipWhitespace.advanceCursor
-  //     )
+  private def readIdentifier(relativePos: Int = 0, next: Lexer = this.next): (Lexer, String) =
+    if !next.getChar.isLetter then
+      next -> input.substring(this.cursor, this.cursor + relativePos + 1)
+    else this.readIdentifier(relativePos + 1, next.advanceCursor)
+
+  private def readNumber(relativePos: Int = 0, next: Lexer = this.next): (Lexer, Token.INT) =
+    if !next.getChar.isDigit then
+      next -> Token.INT { input.substring(this.cursor, this.cursor + relativePos + 1).toInt }
+    else this.readNumber(relativePos + 1, next.advanceCursor)
 
 object Lexer:
   def from(input: String): Lexer = Lexer(input).skipWhitespace
 
 extension (item: Char)
-  def isAsciiWhitespace: Boolean = item match
+  private def isAsciiWhitespace: Boolean = item match
     case ' ' | '\n' | '\t' | '\r' => true
     case _                        => false
 
-  def isMaybeCharNeedToSkip = item match
-    case '=' | '!' => true
-    case _         => false
+private type CodeLiteral = '+' | '-' | '/' | '*' | '<' | '>' | '(' | ')' | '{' | '}' | ',' | ';'
+extension (item: CodeLiteral)
+  private def convertCharOfCodeToToken =
+    item match
+      case '+' => Token.PLUS
+      case '-' => Token.MINUS
+      case '/' => Token.SLASH
+      case '*' => Token.ASTERISK
+      case '<' => Token.LT
+      case '>' => Token.GT
+      case '(' => Token.LPAREN
+      case ')' => Token.RPAREN
+      case '{' => Token.LBRACE
+      case '}' => Token.RBRACE
+      case ',' => Token.COMMA
+      case ';' => Token.SEMICOLON
--- src/test/scala/LexerTest.scala
+++ src/test/scala/LexerTest.scala
@@ -14,7 +14,7 @@ class LexerTest extends munit.FunSuite {
         Token.COMMA,
         Token.ASSIGN,
         Token.SEMICOLON,
-        Token.ILLEGAL
+        Token.IDENT("p")
       ).zipWithIndex
     ) {
       val (nextLexer, curToken) = lexer.getToken
@@ -28,6 +28,7 @@ class LexerTest extends munit.FunSuite {
     lexer = nextLexer
 
   }
+
   test("==を含む") {
     val input = " === +(  ){ },;p"
     var lexer = Lexer.from(input)
@@ -43,7 +44,7 @@ class LexerTest extends munit.FunSuite {
         Token.RBRACE,
         Token.COMMA,
         Token.SEMICOLON,
-        Token.ILLEGAL
+        Token.IDENT("p")
       ).zipWithIndex
     ) {
       val (nextLexer, curToken) = lexer.getToken
@@ -59,7 +60,7 @@ class LexerTest extends munit.FunSuite {
   }
 
   test("！=を含む") {
-    val input = " != +( ==0 ){ },;p"
+    val input = " != +( == ){ },;p"
     var lexer = Lexer.from(input)
 
     for (
@@ -68,13 +69,131 @@ class LexerTest extends munit.FunSuite {
         Token.PLUS,
         Token.LPAREN,
         Token.EQ,
-        Token.ILLEGAL,
         Token.RPAREN,
         Token.LBRACE,
         Token.RBRACE,
         Token.COMMA,
         Token.SEMICOLON,
-        Token.ILLEGAL
+        Token.IDENT("p")
+      ).zipWithIndex
+    ) {
+      val (nextLexer, curToken) = lexer.getToken
+
+      assertEquals(curToken, token)
+      lexer = nextLexer
+    }
+    val (nextLexer, curToken) = lexer.getToken
+
+    assertEquals(curToken, Token.EOF)
+    lexer = nextLexer
+
+  }
+
+  test("read identifier") {
+    val input = " let x = 53; "
+    var lexer = Lexer.from(input)
+
+    for (
+      (token, index) <- Seq(
+        Token.LET,
+        Token.IDENT("x"),
+        Token.ASSIGN,
+        Token.INT(53),
+        Token.SEMICOLON
+      ).zipWithIndex
+    ) {
+      val (nextLexer, curToken) = lexer.getToken
+
+      println(curToken)
+      assertEquals(curToken, token)
+      lexer = nextLexer
+    }
+    val (nextLexer, curToken) = lexer.getToken
+
+    assertEquals(curToken, Token.EOF)
+    lexer = nextLexer
+  }
+
+  test("ソースコード") {
+    import Token.*
+    val input = scala.io.Source.fromResource("sample.monkey").getLines.mkString
+    var lexer = Lexer.from(input)
+
+    for (
+      (token, index) <- Seq(
+        LET,
+        IDENT("five"),
+        ASSIGN,
+        INT(5),
+        SEMICOLON,
+        LET,
+        IDENT("ten"),
+        ASSIGN,
+        INT(10),
+        SEMICOLON,
+        LET,
+        IDENT("add"),
+        ASSIGN,
+        FUNCTION,
+        LPAREN,
+        IDENT("x"),
+        COMMA,
+        IDENT("y"),
+        RPAREN,
+        LBRACE,
+        IDENT("x"),
+        PLUS,
+        IDENT("y"),
+        SEMICOLON,
+        RBRACE,
+        SEMICOLON,
+        LET,
+        IDENT("result"),
+        ASSIGN,
+        IDENT("add"),
+        LPAREN,
+        IDENT("five"),
+        COMMA,
+        IDENT("ten"),
+        RPAREN,
+        SEMICOLON,
+        BANG,
+        MINUS,
+        SLASH,
+        ASTERISK,
+        INT(5),
+        SEMICOLON,
+        INT(5),
+        LT,
+        INT(10),
+        GT,
+        INT(5),
+        SEMICOLON,
+        IF,
+        LPAREN,
+        INT(5),
+        LT,
+        INT(10),
+        RPAREN,
+        LBRACE,
+        RETURN,
+        TRUE,
+        SEMICOLON,
+        RBRACE,
+        ELSE,
+        LBRACE,
+        RETURN,
+        FALSE,
+        SEMICOLON,
+        RBRACE,
+        INT(10),
+        EQ,
+        INT(10),
+        SEMICOLON,
+        INT(10),
+        NotEQ,
+        INT(9),
+        SEMICOLON
       ).zipWithIndex
     ) {
       val (nextLexer, curToken) = lexer.getToken
