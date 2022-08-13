e9ce69963d94f9435d1dee02e168e3b5e8746de5
mm1995tk <jimi.woodstock1995@gmail.com>
2022-08-13 21:30:56 +0900

    return文のテスト追加

--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -1,29 +1,53 @@
 import lexer.Lexer
 import parser.Parser
 import ast.Statement
+import token.Token
+
 class ParserTest extends munit.FunSuite {
 
   test("let文のテスト") {
-
-    val input = f"let x = 5;\nlet y = 10;\nlet foobar = 838383;"
+    val input = "let x = 5;\nlet y = 10;\nlet foobar = 838383;"
     val parser = Parser(Lexer(input))
     val stmts = parser.parseProgram
 
     if stmts.length != 3 then {
       println(s"statementsの要素が3でない: ${stmts.length}")
-      assertEquals(true, false)
+      assert(false)
     }
 
     for ((stmt, ident) <- stmts.zip(Seq("x", "y", "foobar"))) {
-
-      contentOfTestLetStatements(stmt, ident) match
-        case None => assertEquals(true, true)
-        case Some(err) =>
-          println(err)
+      assert {
+        contentOfTestLetStatements(stmt, ident) match
+          case None => true
+          case Some(err) =>
+            println(err)
+            false
+      }
     }
 
   }
-  // test("return文のテスト") {}
+
+  test("return文のテスト") {
+    val input = "return  5;\nreturn 10;\nreturn 838383;"
+    val parser = Parser(Lexer(input))
+    val stmts = parser.parseProgram
+
+    if stmts.length != 3 then {
+      println(s"statementsの要素が3でない: ${stmts.length}")
+      assert(false)
+    }
+
+    for (stmt <- stmts) {
+
+      assert {
+        stmt match
+          case Statement.RETURN(_) => true
+          case _                   => false
+      }
+
+    }
+
+  }
 
 }
 
