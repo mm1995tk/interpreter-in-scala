1f3cc3bf74d5f48cecdb85df127df875727ffca7
mm1995tk <jimi.woodstock1995@gmail.com>
2022-11-05 05:27:31 +0900

    中置式の評価

--- src/main/scala/Evaluator.scala
+++ src/main/scala/Evaluator.scala
@@ -6,6 +6,7 @@ import obj.*
 import ast.{Statement, Expr}
 import token.Token
 import parser.{ParserError, ParserErrors}
+import java.util.Objects
 
 object Evaluator:
   def apply(
@@ -21,6 +22,7 @@ private def evalExpr(expr: Expr): Object = expr match
   case Expr.Int(Token.Int(v)) => Object.Int(v)
   case Expr.Bool(t)           => Object.Boolean(t.equals(Token.True))
   case expr: Expr.Prefix      => evalPrefixExpr(expr)
+  case expr: Expr.Infix       => evalInfixExpr(expr)
   case _                      => ???
 
 private def evalPrefixExpr(item: Expr.Prefix) =
@@ -37,5 +39,54 @@ private def evalPrefixExpr(item: Expr.Prefix) =
         case Object.Boolean(b) => Object.Boolean(!b)
         case _                 => Object.Boolean(true)
     case _ => ConstNull
+
+private def evalInfixExpr(item: Expr.Infix): Object =
+  val expOfL = evalExpr(item.left)
+  val expOfR = evalExpr(item.right)
+  item.token match
+    case t: (Token.Plus.type | Token.Asterisk.type) =>
+      (expOfL, expOfR) match
+        case (Object.Int(l), Object.Int(r)) =>
+          val result = t match
+            case Token.Plus     => l + r
+            case Token.Asterisk => l * r
+          Object.Int(result)
+        case (Object.Boolean(l), Object.Boolean(r)) =>
+          val result = t match
+            case Token.Plus     => l || r
+            case Token.Asterisk => l && r
+          Object.Boolean(l || r)
+        case _ => ConstNull
+
+    case t: (Token.Minus.type | Token.Slash.type) =>
+      (expOfL, expOfR) match
+        case (Object.Int(l), Object.Int(r)) =>
+          val result = t match
+            case Token.Minus => l - r
+            case Token.Slash => l / r
+          Object.Int(result)
+        case _ => ConstNull
+
+    case t: (Token.Lt.type | Token.Gt.type) =>
+      (expOfL, expOfR) match
+        case (Object.Int(l), Object.Int(r)) =>
+          Object.Boolean {
+            t match
+              case Token.Lt => l < r
+              case Token.Gt => l > r
+          }
+        case _ => ConstNull
+
+    case t: (Token.Eq.type | Token.NotEq.type) =>
+      val result = for {
+        l <- expOfL.get
+        r <- expOfR.get
+        value = t match
+          case Token.Eq    => l == r
+          case Token.NotEq => l != r
+      } yield Object.Boolean(value)
+      result getOrElse ConstNull
+
+    case Token.LeftParen => ???
 
 private val ConstNull = obj.Object.Null
--- src/main/scala/Object.scala
+++ src/main/scala/Object.scala
@@ -1,6 +1,11 @@
 package obj
 
 enum Object:
+  def get = this match
+    case Int(value)     => Some(value)
+    case Boolean(value) => Some(value)
+    case Null           => None
+
   case Int(value: scala.Int)
   case Boolean(value: scala.Boolean)
   case Null
