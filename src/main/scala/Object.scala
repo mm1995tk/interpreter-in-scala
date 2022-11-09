986032dfeb0d9ced7a4b4026883b2c74f09928e1
mm1995tk <jimi.woodstock1995@gmail.com>
2022-11-10 03:16:52 +0900

    曖昧な型名を修正

--- src/main/scala/Object.scala
+++ src/main/scala/Object.scala
@@ -3,7 +3,7 @@ package obj
 import token.{InfixToken}
 
 enum Object:
-  def get: Option[Type] = this match
+  def get: Option[MonkeyPrimitiveType] = this match
     case Int(value)         => Some(value)
     case Boolean(value)     => Some(value)
     case ReturnValue(value) => Some(value)
@@ -11,10 +11,10 @@ enum Object:
 
   case Int(value: scala.Int)
   case Boolean(value: scala.Boolean)
-  case ReturnValue(value: Type)
+  case ReturnValue(value: MonkeyPrimitiveType)
   case Null
 
-type Type = scala.Int | scala.Boolean
+type MonkeyPrimitiveType = scala.Int | scala.Boolean
 
 enum EvalError:
   case TypeMismatch(left: Object, right: Object, op: InfixToken)
