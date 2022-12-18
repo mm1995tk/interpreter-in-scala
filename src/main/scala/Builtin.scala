02ef7076b6fc2a58f6f5ba6230dac05271485610
mm1995tk <jimi.woodstock1995@gmail.com>
2022-12-18 18:50:37 +0900

    リファクタリング

--- src/main/scala/Builtin.scala
+++ src/main/scala/Builtin.scala
@@ -1,19 +1,10 @@
 package evaluator.builtin
 
 import obj.{Object, MonkeyPrimitiveType}
-import env.Env
 import scala.collection.immutable.HashMap
 import evaluator.{Evaluator, EvalError}
 
 case class Builtin(f: Seq[Object] => Evaluator[Object], cntOfArgs: scala.Int)
-
-object Builtin:
-  def getEnv: Env =
-    HashMap[String, MonkeyPrimitiveType](
-      "len" -> Object.BuiltinObj(len),
-      "head" -> Object.BuiltinObj(head),
-      "tail" -> Object.BuiltinObj(tail)
-    )
 
 def len: Builtin = Builtin(
   {
--- src/main/scala/Env.scala
+++ src/main/scala/Env.scala
@@ -2,9 +2,12 @@ package env
 
 import obj.{MonkeyPrimitiveType, Object}
 import scala.collection.immutable.HashMap
-import evaluator.builtin.Builtin
+import evaluator.builtin.*
 
 type Env = HashMap[String, MonkeyPrimitiveType]
 
-object Env:
-  def apply(): Env = Builtin.getEnv
+def Env(): Env = HashMap[String, MonkeyPrimitiveType](
+  "len" -> Object.BuiltinObj(len),
+  "head" -> Object.BuiltinObj(head),
+  "tail" -> Object.BuiltinObj(tail)
+)
