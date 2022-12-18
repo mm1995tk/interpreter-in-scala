5ee72a01c8b05c0e957484551609071eb080586f
mm1995tk <jimi.woodstock1995@gmail.com>
2022-12-18 18:19:28 +0900

    組み込み関数head,tailを実装

--- src/main/scala/Builtin.scala
+++ src/main/scala/Builtin.scala
@@ -9,11 +9,11 @@ case class Builtin(f: Seq[Object] => Evaluator[Object], cntOfArgs: scala.Int)
 
 object Builtin:
   def getEnv: Env =
-    new HashMap[String, MonkeyPrimitiveType]()
-      .updated(
-        "len",
-        Object.BuiltinObj(len)
-      )
+    HashMap[String, MonkeyPrimitiveType](
+      "len" -> Object.BuiltinObj(len),
+      "head" -> Object.BuiltinObj(head),
+      "tail" -> Object.BuiltinObj(tail)
+    )
 
 def len: Builtin = Builtin(
   {
@@ -21,6 +21,28 @@ def len: Builtin = Builtin(
       h match
         case Object.Arr(elems) => Evaluator.pure(Object.Int(elems.length))
         case Object.Str(str)   => Evaluator.pure(Object.Int(str.length))
+        case other             => Evaluator.pureErr(???)
+    case seq => Evaluator.pureErr(EvalError.CountOfArgsMismatch(seq.length, 1))
+  },
+  1
+)
+
+def head: Builtin = Builtin(
+  {
+    case h :: Seq() =>
+      h match
+        case Object.Arr(elems) => Evaluator.pure(elems.headOption.getOrElse(Object.Null))
+        case other             => Evaluator.pureErr(???)
+    case seq => Evaluator.pureErr(EvalError.CountOfArgsMismatch(seq.length, 1))
+  },
+  1
+)
+
+def tail: Builtin = Builtin(
+  {
+    case h :: Seq() =>
+      h match
+        case Object.Arr(elems) => Evaluator.pure(Object.Arr(elems.tail))
         case other             => Evaluator.pureErr(???)
     case seq => Evaluator.pureErr(EvalError.CountOfArgsMismatch(seq.length, 1))
   },
